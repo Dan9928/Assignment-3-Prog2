1ce9076222b5576b0276cd8d57245cce0247d9fd
Dan9928 <danielr9928@gmail.com>
2024-03-31 21:40:10 -0600

    SampleController Java docs
    Sample controller java docs

--- Assignment 3/src/application/SampleController.java
+++ Assignment 3/src/application/SampleController.java
@@ -26,110 +26,89 @@ import model.Puzzles;
 import model.Toy;
 
 public class SampleController implements Initializable{
-	
+	//Initializes ToyManager
 	private ToyManager toysManager;
-	
+	/**
+	* Sample controller initializer
+	*/
     public SampleController() throws Exception {
     	toysManager = new ToyManager();
     }
-    	
+    //Initializing FX labels
     @FXML
     private Button buyButton;
-    
     @FXML
     private Label buyID;
-
     @FXML
     private Button clearButton;
-
     @FXML
     private TextField enterName;
-
     @FXML
     private TextField enterSN;
-
     @FXML
     private TextField enterType;
-
     @FXML
     private ToggleGroup group;
-
     @FXML
-    private ListView<String> listToys;
-    
+    private ListView<String> listToys;    
     @FXML
     private Button searchButton;
-
     @FXML
     private RadioButton searchNameButton;
-
     @FXML
     private RadioButton searchSNButton;
-
     @FXML
-    private RadioButton searchTypeButton;
-    
+    private RadioButton searchTypeButton;   
     @FXML
-    private ComboBox<String> categoryComboBox;
-    
+    private ComboBox<String> categoryComboBox;    
     @FXML
-    private Button removeToyButton;
-    
+    private Button removeToyButton;    
     @FXML
     private Button addToyButton;
-
     @FXML
-    private TextField deleteBySN;
-    
+    private TextField deleteBySN;  
     @FXML
-    private ListView<String> deleteToysList;
-    
+    private ListView<String> deleteToysList;  
     @FXML
-    private TextField addType;
-    
+    private TextField addType;    
     @FXML
     private TextField addAge;
-
     @FXML
     private TextField addBrand;
-
     @FXML
     private TextField addClass;
-
     @FXML
     private TextField addCount;
-
     @FXML
     private TextField addDesigner;
-
     @FXML
     private TextField addMaterial;
-
     @FXML
     private TextField addMaximum;
-
     @FXML
     private TextField addMinimum;
-
     @FXML
     private TextField addName;
-
     @FXML
     private TextField addPrice;
-
     @FXML
     private TextField addSN;
-
     @FXML
     private TextField addSize;
-
-
+    //Action event Listeners:
     
+    /**
+    * Buy Toy action. The selected Toy is checked, if its count is greater than 1 it will
+    * subtract the count by one.
+    *
+    * @param event  Buy button click, 
+    *
+    */
     @FXML 
     void buyToy(ActionEvent event) throws IOException {
         String userItem = listToys.getSelectionModel().getSelectedItem();
         boolean itemFound = false;
-        
+        //Iterate through Array of toys
         for (Toy toy : toysManager.getToysList()) {
             if (toy.toString().equals(userItem)) {
                 int count = toy.getCount();
@@ -140,15 +119,22 @@ public class SampleController implements Initializable{
                     int index = listToys.getSelectionModel().getSelectedIndex();
                     listToys.getItems().set(index, toy.toString());
                 } else {
+                	//If toy does not exist then out of stock
                     buyID.setText("Out of stock!");
                 }
                 break;
             }
         }
+        //If for whatever reason item was not found.
         if (!itemFound) {
             buyID.setText("Out of stock!");
         }
     }
+    /**
+    * Clears search preferences.
+    *
+    * @param event Clear button being pressed
+    */
     @FXML
     void clearToys(ActionEvent event) {
     	buyID.setText("");
@@ -163,12 +149,20 @@ public class SampleController implements Initializable{
             listToys.getItems().addAll(toy.toString());
         }
     }
+    /**
+    * Search toys. Updates list view with toys relevant to search.
+    *
+    * @param event  Search button being pressed
+    */
     @FXML
     void searchToys(ActionEvent event) {
+    	//Gather data from fields
     	String name = enterName.getText();
     	String type = enterType.getText();
     	String SN = enterSN.getText();
+    	//Clear list
     	listToys.getItems().clear();
+    	//Searching by name
         if (searchNameButton.isSelected()) {
 			for(Toy toy : toysManager.getToysList()) {
 				 if(toy.getName().toLowerCase().contains(name.toLowerCase())) {
@@ -176,6 +170,7 @@ public class SampleController implements Initializable{
 			        }
 			}
     	}
+        //Searching by SN
         if(searchSNButton.isSelected()) {
 			for(Toy toy : toysManager.getToysList()) {
 				if(toy.getSn().equals(SN)) {
@@ -183,6 +178,7 @@ public class SampleController implements Initializable{
 				}
 			}
         }
+        //Searching by toy type
         if(searchTypeButton.isSelected()) {
     		for (Toy toys : toysManager.getToysList()) {
     	        if (toys instanceof Animals && type.toLowerCase().equalsIgnoreCase("animal")) {
@@ -200,9 +196,14 @@ public class SampleController implements Initializable{
     	    }	
         }
     }
+    /**
+    * Adds toy to store.
+    *
+    * @param event  Add toy button being clicked. Adds toy based on type selected and information entered.
+    */
     @FXML
 	void addToy(ActionEvent event) throws Exception {
-    	
+    	//Gather info from fields
     	String SN = addSN.getText();
     	String Name = addName.getText();
     	String Brand = addBrand.getText();	
@@ -217,10 +218,8 @@ public class SampleController implements Initializable{
     	String Maximum = addMaximum.getText();
     	String Designer = addDesigner.getText();
     	String Players = Minimum + "-" + Maximum;
-    	
-    	
-    	String ToyType = categoryComboBox.getValue();
-    		
+    	//Determine toy type selected
+    	String ToyType = categoryComboBox.getValue();	
     	if(ToyType.equals("Animal")) {
     		Animals toyAdded = new Animals(SN, Name, Brand, Price, Count, Age, Material, Size);
     		toysManager.addToy(toyAdded);
@@ -237,28 +236,44 @@ public class SampleController implements Initializable{
     		Boardgames toyAdded = new Boardgames(SN, Name, Brand, Price, Count, Age, Players, Designer);
     		toysManager.addToy(toyAdded);
     	}
-	
 	}
+    /**
+    * Shows toys list in list view
+    */
     @FXML
     void showToys(ActionEvent event) {
-    	
     }
-    
+    /**
+    * Saves store inventory
+    *
+    * @param event Whenever an action is taken the list is saved
+    */
     @FXML
     private void Save(MouseEvent event) throws IOException {
     	toysManager.saveToFile(toysManager.getToysList());
     }
-
+    /**
+    * Initialize list view and combo boxes
+    *
+    * @param arg0  the string to display.  If the text is null, 
+    *        
+    * @param arg1 Local objects needed for rendering.
+    */
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
             for (Toy toy : toysManager.getToysList()) {
                 listToys.getItems().addAll(toy.toString());
-                
             }       
             categoryComboBox.setEditable(false);
             categoryComboBox.getItems().addAll("Animal", "Puzzle", "Figure", "Boardgame");   
 	}
-	
+	/**
+	    * Initialize list view and combo boxes
+	    *
+	    * @param arg0  the string to display.  If the text is null, 
+	    *        
+	    * @param arg1 Local objects needed for rendering.
+	    */
 	@FXML
 	void removeToy(ActionEvent event) {
 		
